2be16bf0712c6e01c7590ce07b999de1cb7610df
sourleangchhean <sourleangchhean168@gmail.com>
2019-07-22 23:10:22 +0700

    Done with design profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,63 @@
 
 @section('content')
 <div class="container">
-    <div class="col-3">
-        
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="https://i.stack.imgur.com/nhU58.jpg" class="rounded-circle" />
     </div>
+    <div class="col-9 pt-5">
+        <div>
+            <h1>Sour Leangchhean</h1>
+        </div>
+        <div class="d-flex">
+            <div class="pr-4"><strong>190</strong> posts</div>
+            <div class="pr-4"><strong>23K</strong> followers</div>
+            <div class="pr-4"><strong>229K</strong> followings</div>
+        </div>
+        <div class="pt-4 font-weight-bold">
+            <strong>sourleangchhean.com</strong>
+        </div>
+        <div>
+            I love my life. I love my family. I love coding with my tools. @2019
+        </div>
+        <div>www.sourleangchhean.com</div>
+    </div>    
+    </div>
+    <div class="row">
+        <div class="col-4 pt-4">
+            <img src="https://ae01.alicdn.com/kf/HTB17OOXnLuSBuNkHFqDq6xfhVXaN.jpg" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://preview.redd.it/6giqv6zjkog21.jpg?auto=webp&s=bcd2be08e5e557538882e33bb9a44e5a9f2e1f1b" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://pbs.twimg.com/profile_images/596537526539673602/RtwSvvcw.jpg" class="w-100" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4 pt-4">
+            <img src="https://i.chzbgr.com/full/9112752128/h94C6655E/" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://imgaz1.staticbg.com/thumb/view/oaupload/banggood/images/F8/08/5e4dbe77-c7a5-4b16-a181-2c597245ae2b.jpg" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://sc01.alicdn.com/kf/HTB1tileB29TBuNjy0Fcq6zeiFXaN/Super-Cute-Simulation-Cats-Furry-Kittens-Doll.jpg_350x350.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4 pt-4">
+            <img src="https://pbs.twimg.com/profile_images/962170088941019136/lgpCD8X4_400x400.jpg" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://www.thesprucepets.com/thmb/7a8gqPiqt0gA5Pe4I3qR9y-u7FQ=/1080x1080/filters:no_upscale()/29739961_627747617569043_3464262123499028480_n-5b0df689ba617700362ed399.jpg" class="w-100" />
+        </div>
+        <div class="col-4 pt-4">
+            <img src="https://www.benjerry.com/files/live/sites/systemsite/files/whats-new/3586-dogs/dog-1b.jpg" class="w-100" />
+        </div>
+    </div>
+    
 </div>
 @endsection
