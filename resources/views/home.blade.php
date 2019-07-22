dda5a9b01fb57bda74f664ed278c7fbc7e4c42b8
sourleangchhean <sourleangchhean168@gmail.com>
2019-07-22 22:29:34 +0700

    finish migrate new column username to database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="col-3">
+        
     </div>
 </div>
 @endsection
